8532e8924d516fdfa9597fd64bcf2c073a2ab92b
Simon Tse <simon.tse.hk@gmail.com>
2023-12-11 23:57:09 +0800

    for static-healthcheck, always update the "latest" tag

--- static-healthcheck/build.sh
+++ static-healthcheck/build.sh
@@ -6,5 +6,7 @@ cd static-healthcheck
 commitID=$(git rev-parse --short HEAD)
 
 nerdctl build --platform linux/amd64,linux/arm64/v8 --tag minixxie/static-healthcheck:$commitID .
+nerdctl tag minixxie/static-healthcheck:$commitID minixxie/static-healthcheck:latest
 nerdctl login
 nerdctl push --platform linux/amd64,linux/arm64/v8 minixxie/static-healthcheck:$commitID
+nerdctl push --platform linux/amd64,linux/arm64/v8 minixxie/static-healthcheck:latest
