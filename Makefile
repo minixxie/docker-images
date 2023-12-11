1e2cef62a9aa5c74d72b014e3b985b39143dc901
Simon Tse <simon.tse.hk@gmail.com>
2023-12-11 23:50:02 +0800

    Added xxl-job-admin

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ SHELL := /bin/bash
 build:
 	cd eclipse-temurin/19.0.1_10-jdk-mvn && ./build.sh && cd -
 	cd eclipse-temurin/19.0.1_10-jre && ./build.sh && cd -
+	cd static-healthcheck && ./build.sh && cd -
+	cd xxl-job-admin/2.3.1 && ./build.sh && cd -
---
+++ xxl-job-admin/2.3.1/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+scriptPath=$(cd $(dirname "$0") && pwd)
+
+nerdctl build --platform linux/amd64,linux/arm64/v8 --tag minixxie/xxl-job-admin:2.3.1
+nerdctl login
+nerdctl push --platform linux/amd64,linux/arm64/v8 minixxie/xxl-job-admin:2.3.1
