d89fc43aac941965cadc3cc63ab46577f5891700
Simon Tse <simon.tse.hk@gmail.com>
2023-12-11 23:50:58 +0800

    Typo

--- xxl-job-admin/2.3.1/build.sh
+++ xxl-job-admin/2.3.1/build.sh
@@ -2,6 +2,6 @@
 
 scriptPath=$(cd $(dirname "$0") && pwd)
 
-nerdctl build --platform linux/amd64,linux/arm64/v8 --tag minixxie/xxl-job-admin:2.3.1
+nerdctl build --platform linux/amd64,linux/arm64/v8 --tag minixxie/xxl-job-admin:2.3.1 .
 nerdctl login
 nerdctl push --platform linux/amd64,linux/arm64/v8 minixxie/xxl-job-admin:2.3.1
