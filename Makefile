7e7a6d168df3e1e867c55dfd9c17c5f4a565605a
Simon Tse <simon.tse.hk@gmail.com>
2024-04-18 23:18:00 +0800

    Added ragflow

--- Makefile
+++ Makefile
@@ -1,8 +1,10 @@
 SHELL := /bin/bash
 
 build:
+	git submodule update --init
 	cd eclipse-temurin/19.0.1_10-jdk-mvn && ./build.sh && cd -
 	cd eclipse-temurin/19.0.1_10-jre && ./build.sh && cd -
 	cd static-healthcheck && ./build.sh && cd -
 	cd xxl-job-admin/2.3.1 && ./build.sh && cd -
 	cd privateGPT && ./build.sh && cd -
+	cd ragflow && ./build.sh && cd -
---
+++ ragflow/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+push=1
+
+scriptPath=$(cd $(dirname "$0") && pwd)
+
+cd ragflow
+commitID=$(git rev-parse HEAD)
+
+image=minixxie/ragflow-cuda
+platforms=linux/amd64
+nerdctl build . \
+	--namespace=k8s.io \
+	-f ../Dockerfile.cuda \
+	--platform $platforms \
+	--tag $image:$commitID
+nerdctl tag $image:$commitID $image:latest
+
+if [ $push -eq 1 ]; then
+	nerdctl login
+	nerdctl push --platform $platforms $image:$commitID
+	nerdctl push --platform $platforms $image:latest
+fi
+
+image=minixxie/ragflow
+platforms=linux/amd64,linux/arm64/v8
+platforms=linux/amd64
+nerdctl build . \
+	--namespace=k8s.io \
+	-f ../Dockerfile.scratch \
+	--platform $platforms \
+	--tag $image:$commitID
+nerdctl tag $image:$commitID $image:latest
+
+if [ $push -eq 1 ]; then
+	nerdctl login
+	nerdctl push --platform $platforms $image:$commitID
+	nerdctl push --platform $platforms $image:latest
+fi
