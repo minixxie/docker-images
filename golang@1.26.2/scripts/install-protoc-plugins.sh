d1397872ba1615cf7e244c5cec4288a2cbc8df6d
Simon Tse <simon.tse.hk@gmail.com>
2026-05-05 16:37:26 +0800

    Rolled back to openapiv2

--- golang@1.26.2/scripts/install-protoc-plugins.sh
+++ golang@1.26.2/scripts/install-protoc-plugins.sh
@@ -4,4 +4,5 @@ go install \
 	google.golang.org/protobuf/cmd/protoc-gen-go \
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc \
 	github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+	github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
 	github.com/googleapis/api-linter/cmd/api-linter
