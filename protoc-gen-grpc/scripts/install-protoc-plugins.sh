8ebef515cf20f1a78fa31aa9e3f19162ba2f5652
Simon Tse <simon.tse.hk@gmail.com>
2026-05-21 13:05:12 +0800

    Newer versions for protoc-gen image

--- protoc-gen-grpc/scripts/install-protoc-plugins.sh
+++ protoc-gen-grpc/scripts/install-protoc-plugins.sh
@@ -2,6 +2,6 @@
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.36.10
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.3.0
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.18.0
-go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.18.0
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@v2.29.0
+go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@v2.29.0
 go install github.com/googleapis/api-linter/cmd/api-linter@v1.72.0
